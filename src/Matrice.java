644ca358366043218d6c810324d9f2717265a9ec
Atto Deba Souleymane <souleymane.attodeba@etudiant.univ-lille1.fr>
2017-09-29 02:41:06 +0200

    mtt and mtt-binaire is done

--- src/Matrice.java
+++ src/Matrice.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Matrice {
     public ArrayList<String> userList=  new ArrayList<>();
--- src/MatriceBinaire.java
+++ src/MatriceBinaire.java
@@ -9,7 +9,7 @@ public class MatriceBinaire extends Matrice {
     /**
      * matrice de distance entre les themes
      */
-    public  int [][] Mtt;
+    public  double [][] Mtt;
     /**
      * transformation binaire de @Mtt
      */
@@ -23,6 +23,10 @@ public class MatriceBinaire extends Matrice {
         super(initfilename);
         this.readMUTFile("matriceMUT.txt");
         this.MutBinaire= this.toBinaire(this.matriceMUTA);
+        this.Mtt = new double[this.nbCustomer][this.nbCustomer];
+        this.MttBinaire = new int[this.nbCustomer][this.nbCustomer];
+        this.toMtt();
+        this.mttToBinaire(0.5);
         this.toFile("matriceMUTBinaire.txt");
     }
     public int [][] toBinaire(final int[][] matrice){
@@ -69,10 +73,56 @@ public class MatriceBinaire extends Matrice {
         catch (IOException exception){ System.out.println ("Errors : " + exception.getMessage());}
 
     }
+    private  int getIntersection(int customer1, int customer2){
+        int intersection=0;
+        for (int j = 0; j < this.nbTheme ; j++) {
+            if( (this.MutBinaire[customer1][j]==1)  && (this.MutBinaire[customer2][j]==1) )
+                          intersection++;
+        }
+        System.out.println ("intersection == "+intersection);
+        return  intersection;
+    }
+    private   int getUnion(int customer1, int customer2){
+        int union=0;
+        for (int j = 0; j < this.nbTheme ; j++) {
+            if(this.MutBinaire[customer1][j]==1  || this.MutBinaire[customer2][j]==1)
+            union++;
+        }
+        System.out.println ("union == "+union);
+        return  union;
+    }
+    private double getDistance(int customer1, int customer2){
+          double intersection=getIntersection(customer1,customer2);
+                  double union=getUnion(customer1, customer2);
+                  System.out.println("distance"+( 1-(2/3.1) ));
+        return  (1- (intersection / union) );
+    }
+    private void initMtt(){
+        for (int i = 0; i <nbCustomer ; i++) {
+            for (int j = 0; j <nbCustomer ; j++) {
+                this.Mtt[i][j]=0.0;
+            }
+        }
+    }
+    public  void  toMtt(){
+        this.initMtt();
+        for (int i = 0; i < nbCustomer ; i++) {
+            for (int j = i; j < nbCustomer; j++) {
+                this.Mtt[i][j] = getDistance(i, j);
+            }
+        }
+    }
+    public  void mttToBinaire( double marge){
+        for (int i = 0; i <nbCustomer ; i++) {
+            for (int j = i; j <nbCustomer ; j++) {
+                this.MttBinaire[i][j] =  (this.Mtt[i][j]<=marge)? 1:0;
+            }
+        }
 
+    }
     public void toFile( String filenameMUTB){
-       /// File themeFile   = new File ("theme.txt");
-      //  File userFile    =   new File ("user.txt");
+        File mttBinaireFile   = new File ("matriceMttBinaire.txt");
+        File mttFile    =   new File ("matriceMtt.txt");
         File matriceMUTBFile = new File (filenameMUTB);
 
         try
@@ -86,8 +136,22 @@ public class MatriceBinaire extends Matrice {
             }
             tpw.close();
 
-
-
+            tpw = new PrintWriter (new BufferedWriter (new FileWriter (mttFile)));
+            for (int i = 0; i <nbCustomer ; i++) {
+                for (int j = 0; j <nbCustomer; j++) {
+                    tpw.print(Mtt[i][j]+ ";");
+                }
+                tpw.println("");
+            }
+            tpw.close();
+            tpw = new PrintWriter (new BufferedWriter (new FileWriter (mttBinaireFile)));
+            for (int i = 0; i <nbCustomer ; i++) {
+                for (int j = 0; j <nbCustomer; j++) {
+                    tpw.print(MttBinaire[i][j]+ ";");
+                }
+                tpw.println("");
+            }
+            tpw.close();
         }
         catch (IOException exception)
         {
